895ae8e78371fc8ec5ac24e19dbb0a24d10a66be
Jürgen Wewer <juergen.wewer@gmail.com>
2025-08-27 15:11:16 +0200

    initial commit

--- script.sh
+++ script.sh
@@ -4,9 +4,9 @@ done
 
 git ls-remote --heads github
 
-git for-each-ref --format='%(refname:lstrip=3)' \
-  --exclude='refs/remotes/origin/HEAD' refs/remotes/origin |
-while IFS= read -r br; do
+git for-each-ref --format='%(refname:lstrip=3)' refs/remotes/origin \
+| grep -v '^HEAD$' \
+| while IFS= read -r br; do
   echo "Pushe $br …"
   git push github "refs/remotes/origin/$br:refs/heads/$br" || break
 done
