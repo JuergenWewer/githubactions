9443856a327c464f237670da6221b2e3b1e7dbc9
Jürgen Wewer <juergen.wewer@gmail.com>
2025-08-27 15:04:45 +0200

    initial commit

--- script.sh
+++ script.sh
@@ -3,3 +3,10 @@ for br in $(git for-each-ref --format='%(refname:short)' refs/heads); do
 done
 
 git ls-remote --heads github
+
+git for-each-ref --format='%(refname:lstrip=3)' \
+  --exclude='refs/remotes/origin/HEAD' refs/remotes/origin |
+while IFS= read -r br; do
+  echo "Pushe $br …"
+  git push github "refs/remotes/origin/$br:refs/heads/$br" || break
+done
